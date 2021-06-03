26bc847fe504f2d2952712afafabc2cb12f13769
eduardmelkonyan <melkon1999@gmail.com>
2021-06-03 12:50:41 +0400

    first commit

---
+++ reverse.js
@@ -0,0 +1,12 @@
+let arr = [1,2,3,4,5];
+function reverse(arr){
+    let j = arr.length - 1;
+    for(let i = 0; i < arr.length/2; ++i){
+        let tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    --j;
+    }
+}
+reverse(arr);
+console.log(arr);
